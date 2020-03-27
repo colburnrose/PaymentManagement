612a70bd08fb3f8c42d8efaf03bafa03b8650586
colburnrose <colburnsanders@gmail.com>
2020-03-27 14:39:55 -0500

    create viewmodel, create action - PaymentController

--- PaymentManagement.Web/Models/PaymentViewModel.cs
+++ PaymentManagement.Web/Models/PaymentViewModel.cs
@@ -28,4 +28,48 @@ namespace PaymentManagement.Web.Models
         public decimal NetPayment { get; set; }
 
     }
+
+    public class PaymentCreateModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Full Name")]
+        public int EmpId { get; set; }
+        public Employee Employee { get; set; }
+        public string FullName { get; set; }
+        public string NINO { get; set; }
+        [DataType(DataType.Date), Display(Name = "Pay Date")]
+        public DateTime DatePaid { get; set; } = DateTime.UtcNow;
+        [Display(Name = "Month Paid")]
+        public string PayMonth { get; set; } = DateTime.Today.Month.ToString();
+        [Display(Name = "Tax Year")]
+        public int TaxYearId { get; set; }
+        public TaxYear TaxYear { get; set; }
+        public string TaxCode { get; set; } = "1250L"; // default tax code
+        [Display(Name = "Hourly Rate")]
+        public decimal HourlyRate { get; set; }
+        [Display(Name = "Hours Worked")]
+        public decimal HoursWorked { get; set; }
+        [Display(Name = "Contract Hours")]
+        public decimal ContractHours { get; set; }
+        [Display(Name = "Overtime Hours")]
+        public decimal OvertimeHours { get; set; }
+        [Display(Name = "Contract Earnings")]
+        public decimal ContractEarnings { get; set; }
+        [Display(Name = "Overtime Earnings")]
+        public decimal OvertimeEarnings { get; set; }
+        [Display(Name = "Tax")]
+        public decimal Tax { get; set; }
+        [Display(Name = "SSN")]
+        public decimal SSN { get; set; }
+        [Display(Name = "Union Fee")]
+        public decimal? UnionFee { get; set; }
+        [Display(Name = "Student Loan")]
+        public decimal? StudentLoan { get; set; }
+        [Display(Name = "Total Earnings")]
+        public decimal TotalEarnings { get; set; }
+        [Display(Name = "Total Deduction")]
+        public decimal TotalDeduction { get; set; }
+        [Display(Name = "Net Pay")]
+        public decimal NetPayment { get; set; }
+    }
 }
