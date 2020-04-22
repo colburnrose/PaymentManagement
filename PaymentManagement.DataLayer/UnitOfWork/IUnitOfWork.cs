2f765c396c700aedb09805f2eb6f32235794ee5f
colburnrose <colburnsanders@gmail.com>
2020-04-22 11:04:51 -0500

    scaffold repository/uow

---
+++ PaymentManagement.DataLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentManagement.DataLayer.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int Complete();
+    }
+}
